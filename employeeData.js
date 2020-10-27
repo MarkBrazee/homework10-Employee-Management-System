bc200a0ff4f180e282c9606f2546de6904537248
Mark Brazee <m.brazee@comcast.net>
2020-10-26 20:40:26 -0600

    first draft of js file connection and inquirer

--- employeeData.js
+++ employeeData.js
@@ -2,6 +2,8 @@
     // Create js file for Inquirer and comnection functions.
         // require Inquirer and fs
         // require mysql
+        // create connection to sql file
+        // create gitignore file for node modules
 
     //Create three SQL tables 
         // Create seed.sql file
@@ -23,6 +25,8 @@
             // manager_id- INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
 
     // Create a command-line application (Inquirer) for User input
+    // Write code to use inquirer to gather information about the development team members,
+        // create objects for each team member
         // Add departments, roles, employees
         // View departments, roles, employees
         // Update employee roles
@@ -37,15 +41,15 @@
         // Constructor function helpful in organization?
 //==================================================
 
+// Create js file for Inquirer and comnection functions.
+
 const inquirer = require("inquirer")
 const fs = require("fs");
         
-
 var mysql = require("mysql");
 
-
-
-
+// Connection to employeeDB
+//==================================================
 var connection = mysql.createConnection({
     host: "localhost",
   
@@ -57,7 +61,7 @@ var connection = mysql.createConnection({
   
     // Your password
     password: "2005*alyKat",
-    database: "ice_creamDB"
+    database: "employeeDB"
   });
   
   connection.connect(function(err) {
@@ -73,3 +77,119 @@ var connection = mysql.createConnection({
       connection.end();
     });
   }
+
+// Write code to use inquirer to gather information about the development team members
+
+// Array of questions for each employee
+    // Department Name
+    // Department Id
+    // Role Id
+    // Role title
+    // Role salary
+    // Employee Id
+    // Employee first name
+    // Employee last name
+    // Employee's Manager Id
+
+    const questions = [ {
+
+        type: "input",
+        message: "What department do you work in?",
+        name: "name",
+        },
+    {
+        type: "input",
+        message: "What is your department id?",
+        number: "deptId",
+        },
+    {
+        type: "input",
+        message: "What is your Role Id",
+        number: "roleId",
+        },
+    {
+        message: "What is your Role Title?",
+        type: 'list',
+        choices: [
+            "Engineer",
+            "Manager",
+            "Intern"
+        ],
+        name: "roleTitle"
+    },
+    {
+        type: "input",
+        message: "What is your Salary?",
+        number: "salary",
+        },
+    {
+        type: "input",
+        message: "What is your Employee Id",
+        number: "employeeId",
+        },
+    {
+        type: "input",
+        message: "What is your Employee Id",
+        number: "employeeId",
+        },
+    {
+        type: "input",
+        message: "What is your first name",
+        name: "firstName",
+        },
+        {
+        type: "input",
+        message: "What is your last name",
+        name: "lastName",
+        },
+    {
+        type: "input",
+        message: "What is your Manager's Id number?",
+        number: "mgrIdNum",
+        },
+
+];
+
+// create object for all employees
+
+// function to write to file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data)
+} 
+
+
+function init() {
+    inquirer.prompt(questions)
+    .then(({
+        name,
+        deptId,
+        roleId,
+        roleTitle,
+        salary,
+        employeeId,
+        firstName,
+        lastName,
+        mgrIdNum
+    }) => {
+
+        console.log(name, deptId, roleId, roleTitle, salary, employeeId, firstName, lastName, mgrIdNum)
+
+        switch (type) {
+            case "Engineer":
+                const engineer = new Engineer
+                (name, deptId, roleId, roleTitle, salary, employeeId, firstName, lastName, mgrIdNum)
+                break;
+            case "Manager":
+                const manager = new Manager
+                (name, deptId, roleId, roleTitle, salary, employeeId, firstName, lastName)
+                break;
+            case "Intern":
+                const intern = new Intern
+                (name, deptId, roleId, roleTitle, salary, employeeId, firstName, lastName, mgrIdNum)
+                break;
+        }
+    })
+}
+
+// function call to initialize program
+init();
---
+++ seed.sql
@@ -0,0 +1,7 @@
+-- Drops the favorite_db if it exists currently --
+DROP DATABASE IF EXISTS employee_db;
+-- Creates the "favorite_db" database --
+CREATE DATABASE employee_db;
+
+-- Make it so all of the following code will affect favorite_db --
+Use employee_db;
